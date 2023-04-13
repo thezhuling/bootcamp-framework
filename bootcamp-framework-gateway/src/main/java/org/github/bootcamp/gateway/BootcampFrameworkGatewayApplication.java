6d6e533b9598d3986f8e6b4fd6f1000f6df9e220
zhuling <thezhuling@gmail.com>
2023-04-13 19:57:32 +0800

    init bootcamp-framework-gateway module

---
+++ bootcamp-framework-gateway/src/main/java/org/github/bootcamp/gateway/BootcampFrameworkGatewayApplication.java
@@ -0,0 +1,14 @@
+package org.github.bootcamp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuling
+ */
+@SpringBootApplication
+public class BootcampFrameworkGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampFrameworkGatewayApplication.class, args);
+    }
+}
