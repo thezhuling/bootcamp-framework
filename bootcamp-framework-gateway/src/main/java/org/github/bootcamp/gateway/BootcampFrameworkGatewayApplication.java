10f4d4d0b155c93bed82b77366605f7ca2aaca06
zhuling <thezhuling@gmail.com>
2023-04-17 15:24:33 +0800

    add bootcamp-framework-microservice module

--- bootcamp-framework-gateway/src/main/java/org/github/bootcamp/gateway/BootcampFrameworkGatewayApplication.java
+++ bootcamp-framework-gateway/src/main/java/org/github/bootcamp/gateway/BootcampFrameworkGatewayApplication.java
@@ -2,13 +2,28 @@ package org.github.bootcamp.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author zhuling
  */
 @SpringBootApplication
 public class BootcampFrameworkGatewayApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BootcampFrameworkGatewayApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(BootcampFrameworkGatewayApplication.class, args);
+  }
+
+  @Bean
+  public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+    return builder
+        .routes()
+        .route(
+            p ->
+                p.path("get")
+                    .filters(f -> f.addRequestHeader("Hello", "World"))
+                    .uri("http://localhost:8081"))
+        .build();
+  }
 }
---
+++ bootcamp-framework-microservice/src/main/java/org/github/bootcamp/microservice/BootcampFrameworkMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package org.github.bootcamp.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuling
+ */
+@SpringBootApplication
+public class BootcampFrameworkMicroServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(BootcampFrameworkMicroServiceApplication.class, args);
+  }
+}
